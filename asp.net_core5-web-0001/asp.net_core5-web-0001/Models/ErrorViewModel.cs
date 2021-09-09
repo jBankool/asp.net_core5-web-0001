33d9fc7ea01c564cdd6c8a9cbe82b3e86b8ed3cb
jBankool <32521859+jBankool@users.noreply.github.com>
2021-09-09 09:25:29 +0700

    new Project  web - core 5
    -new Project  web  - core 5

---
+++ asp.net_core5-web-0001/asp.net_core5-web-0001/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace asp.net_core5_web_0001.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
